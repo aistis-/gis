c69c2af3478d9b273e6a1bd6c2f24bc5a4b4c975
Aistis <aistis8624@gmail.com>
2014-05-22 02:22:25 +0300

    Almost finished 4 part

--- src/main/java/org/geotools/gis/Calculations.java
+++ src/main/java/org/geotools/gis/Calculations.java
@@ -5,17 +5,14 @@ import java.text.DecimalFormat;
 import java.util.*;
 
 import com.vividsolutions.jts.geom.Geometry;
-import org.geotools.data.DataUtilities;
 import org.geotools.data.Query;
 import org.geotools.data.collection.ListFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureIterator;
 import org.geotools.data.simple.SimpleFeatureSource;
 import org.geotools.factory.CommonFactoryFinder;
-import org.geotools.feature.FeatureCollections;
 import org.geotools.geometry.jts.ReferencedEnvelope;
 import org.geotools.swing.JMapFrame;
-import org.opengis.feature.type.FeatureType;
 import org.opengis.filter.Filter;
 
 import javax.swing.*;
@@ -42,14 +39,9 @@ public class Calculations {
         SimpleFeatureSource areasSource = App.dataController.mapData.get("sven_PLO_P");
         SimpleFeatureSource polygonsSource = App.dataController.mapData.get("sven_SAV_P");
 
-        SimpleFeatureType schema2 = areasSource.getSchema();
-        String typeName2 = schema2.getTypeName();
-        String geomName2 = schema2.getGeometryDescriptor().getLocalName();
         FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2();
 
         SimpleFeatureType schema = buildingsSource.getSchema();
-        String typeName = schema.getTypeName();
-        String geomName = schema.getGeometryDescriptor().getLocalName();
 
         Filter filter;
 
@@ -60,133 +52,266 @@ public class Calculations {
         }
 
         SimpleFeatureCollection outerFeatures = polygonsSource.getFeatures(filter);
-        SimpleFeatureIterator iterator = outerFeatures.features();
-        SimpleFeatureIterator iteratorJoined;
-        SimpleFeatureIterator iteratorJoined2;
+        SimpleFeatureIterator iterator;
+        SimpleFeature feature;
 
+        String geometryPropertyName = schema.getGeometryDescriptor().getLocalName();
+        CoordinateReferenceSystem targetCRS = schema.getGeometryDescriptor().getCoordinateReferenceSystem();
+
+        ReferencedEnvelope bbox = new ReferencedEnvelope(outerFeatures.getBounds().getMinX(), outerFeatures.getBounds().getMaxX(),
+                outerFeatures.getBounds().getMinY(), outerFeatures.getBounds().getMaxY(), targetCRS);
+
+        filter = ff.bbox(ff.property(geometryPropertyName), bbox);
+
+        SimpleFeatureCollection filteredAreaFeatures = areasSource.getFeatures(filter);
+
+        IntersectionFeatureCollection intersection = new IntersectionFeatureCollection();
+
+        SimpleFeatureCollection intersectedCollection = intersection.execute(
+            filteredAreaFeatures,
+            outerFeatures,
+            null,
+            null,
+            IntersectionFeatureCollection.IntersectionMode.INTERSECTION,
+            false,
+            false
+        );
+
+        iterator = intersectedCollection.features();
+
+        HashMap<String, Double> PLOTAIareas = new HashMap<>();
+
+        while (iterator.hasNext()) {
+            feature = iterator.next();
+
+            String type;
+
+            switch (feature.getAttribute("sven_PLO_P_GKODAS").toString()) {
+                case "hd1":
+                case "hd2":
+                case "hd3":
+                case "hd4":
+                case "hd9":
+                    type = "Hidro";
+                    break;
+                case "ms0":
+                    type = "Woods";
+                    break;
+                case "ms4":
+                    type = "Gardens";
+                    break;
+                case "pu0":
+                    type = "Buildings";
+                    break;
+                default:
+                    type = "Unknown";
+            }
+
+            String key = feature.getAttribute("sven_SAV_P_SAV").toString() + "_" + type;
+
+            if (!PLOTAIareas.containsKey(key)) {
+                PLOTAIareas.put(key, ((Geometry)feature.getDefaultGeometry()).getArea());
+            } else {
+                PLOTAIareas.put(
+                    key,
+                    PLOTAIareas.get(key) + ((Geometry)feature.getDefaultGeometry()).getArea()
+                );
+            }
+
+        }
+
+//
+//        SimpleFeatureCollection intersectedCollection = new IntersectedFeatureCollection(
+//            filteredFeatures,
+//            outerFeatures
+//        );
+
+        SimpleFeatureCollection intersectedCollection2 = intersection.execute(
+                buildingsSource.getFeatures(),
+                intersectedCollection,
+                null,
+                null,
+                IntersectionFeatureCollection.IntersectionMode.INTERSECTION,
+                false,
+                false
+        );
+
+        iterator = intersectedCollection2.features();
         HashMap<String, FeatureInfo> calculations = new HashMap<>();
 
         try {
             while (iterator.hasNext()) {
-                SimpleFeature feature = iterator.next();
-                Geometry geometry = (Geometry) feature.getDefaultGeometry();
+                feature = iterator.next();
 
-                if (!geometry.isValid()) {
-                    // skip bad data
-                    continue;
-                }
-                Filter innerFilter = ff.intersects(ff.property(geomName2), ff.literal(geometry));
-                Query innerQuery = new Query(typeName2, innerFilter, Query.ALL_NAMES);
-                SimpleFeatureCollection join = areasSource.getFeatures(innerQuery);
+                String type;
 
-                System.out.println("Found intersected features: " + join.size());
-
-                SimpleFeatureCollection intersectedCollection = new IntersectedFeatureCollection(
-                    join,
-                    outerFeatures
-                );
-
-                SimpleFeatureIterator iterator2 = intersectedCollection.features();
-                SimpleFeature feature2;
-
-                ArrayList<SimpleFeature> list = new ArrayList<>();
-
-                while (iterator2.hasNext()) {
-                    feature2 = iterator2.next();
-
-                    list.add(feature2);
+                switch (feature.getAttribute("sven_PLO_P_sven_PLO_P_GKODAS").toString()) {
+                    case "hd1":
+                    case "hd2":
+                    case "hd3":
+                    case "hd4":
+                    case "hd9":
+                        type = "Hidro";
+                        break;
+                    case "ms0":
+                        type = "Woods";
+                        break;
+                    case "ms4":
+                        type = "Gardens";
+                        break;
+                    case "pu0":
+                        type = "Buildings";
+                        break;
+                    default:
+                        type = "Unknown";
                 }
 
-                intersectedCollection = DataUtilities.collection(list);
+                String key = feature.getAttribute("sven_SAV_P_sven_SAV_P_SAV").toString() + "_" + type;
 
-//                IntersectionFeatureCollection inter = new IntersectionFeatureCollection();
-//                SimpleFeatureCollection intersectedCollection = inter.execute(
-//                        join,
-//                        outerFeatures,
-//                        null,
-//                        null,
-//                        IntersectionFeatureCollection.IntersectionMode.INTERSECTION,
-//                        false,
-//                        false
-//                );
-
-                iteratorJoined = intersectedCollection.features();
-
-                while (iteratorJoined.hasNext()) {
-                    feature = iteratorJoined.next();
-                    geometry = (Geometry) feature.getDefaultGeometry();
-//                    geometry = (Geometry) feature.getAttribute(0);
-
-                    if (!geometry.isValid()) {
-                        // skip bad data
-                        continue;
-                    }
-                    innerFilter = ff.intersects(ff.property(geomName), ff.literal(geometry));
-                    innerQuery = new Query(typeName, innerFilter, Query.ALL_NAMES);
-                    join = buildingsSource.getFeatures(innerQuery);
-
-                    SimpleFeatureCollection intersectedCollection2 = new IntersectedFeatureCollection(
-                        join,
-                        intersectedCollection
-                    );
-
-                    iteratorJoined2 = intersectedCollection2.features();
-
-                    System.out.println("Found intersected features with buildings: " + join.size());
-
-                    while (iteratorJoined2.hasNext()) {
-                        feature = iteratorJoined2.next();
-
-                        String type;
-
-                        switch (feature.getAttribute(5).toString()) {
-                            case "hd1":
-                            case "hd2":
-                            case "hd3":
-                            case "hd4":
-                            case "hd9":
-                                type = "Hidro";
-                                break;
-                            case "ms0":
-                                type = "Woods";
-                                break;
-                            case "ms4":
-                                type = "Gardens";
-                                break;
-                            case "pu0":
-                                type = "Buildings";
-                                break;
-                            default:
-                                type = "Unknown";
-                        }
-
-//                        try {
-                            String key = feature.getAttribute("sven_SAV_P_SAV").toString() + "_" + type;
-
-                            if (!calculations.containsKey(key)) {
-                                calculations.put(key, new FeatureInfo());
-                            }
-
-                            FeatureInfo featureInfo = calculations.get(key);
-
-                            featureInfo.region = feature.getAttribute("sven_SAV_P_SAV").toString();
-
-                            featureInfo.type = type;
-                            featureInfo.frequency++;
-                            featureInfo.size += ((Geometry) feature.getDefaultGeometry()).getArea();
-                            featureInfo.size += Double.parseDouble(feature.getAttribute("sven_PLO_P_SHAPE_area").toString());
-                            featureInfo.regionPlot += Double.parseDouble(feature.getAttribute("sven_PLO_P_PLOT").toString());
-//                        } catch (NullPointerException e) {
-//                            continue;
-//                        }
-                    }
+                if (!calculations.containsKey(key)) {
+                    calculations.put(key, new FeatureInfo());
                 }
+
+                FeatureInfo featureInfo = calculations.get(key);
+
+                featureInfo.region = feature.getAttribute("sven_SAV_P_sven_SAV_P_SAV").toString();
+
+                featureInfo.type = type;
+                featureInfo.frequency++;
+                featureInfo.size += ((Geometry) feature.getAttribute("sven_PAS_P_the_geom")).getArea();
+                featureInfo.regionPlot = PLOTAIareas.get(key);
             }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             iterator.close();
         }
+
+//        try {
+//            while (iterator.hasNext()) {
+//                SimpleFeature feature = iterator.next();
+//                Geometry geometry = (Geometry) feature.getDefaultGeometry();
+//
+//                if (!geometry.isValid()) {
+//                    // skip bad data
+//                    continue;
+//                }
+//                Filter innerFilter = ff.intersects(ff.property(geomName2), ff.literal(geometry));
+//                Query innerQuery = new Query(typeName2, innerFilter, Query.ALL_NAMES);
+//                SimpleFeatureCollection join = areasSource.getFeatures(innerQuery);
+//
+//                System.out.println("Found intersected features: " + join.size());
+//
+////                IntersectionFeatureCollection2 inter = new IntersectionFeatureCollection2();
+//                SimpleFeatureCollection intersectedCollection = new IntersectedFeatureCollection(
+//                    join,
+//                    outerFeatures
+//                );
+//
+////                SimpleFeatureCollection intersectedCollection = new IntersectedFeatureCollection(
+////                    join,
+////                    outerFeatures
+////                );
+//
+//                SimpleFeatureIterator iterator2 = intersectedCollection.features();
+//                SimpleFeature feature2;
+//                SimpleFeatureType featureType = null;
+//
+//                ArrayList<SimpleFeature> list = new ArrayList<>();
+//
+//                while (iterator2.hasNext()) {
+//                    feature2 = iterator2.next();
+//                    featureType = feature2.getFeatureType();
+//
+//                    list.add(feature2);
+//                }
+//
+//                intersectedCollection = new ListFeatureCollection(featureType, list);
+//                iteratorJoined = intersectedCollection.features();
+//
+//                while (iteratorJoined.hasNext()) {
+//                    feature2 = iteratorJoined.next();
+//                    geometry = (Geometry) feature.getDefaultGeometry();
+//
+//                    if (!geometry.isValid()) {
+//                        // skip bad data
+//                        continue;
+//                    }
+//                    innerFilter = ff.intersects(ff.property(geomName), ff.literal(geometry));
+//                    innerQuery = new Query(typeName, innerFilter, Query.ALL_NAMES);
+//                    join = buildingsSource.getFeatures(innerQuery);
+//
+////                    IntersectionFeatureCollection2 inter2 = new IntersectionFeatureCollection2();
+//                    SimpleFeatureCollection intersectedCollection2 = new IntersectedFeatureCollection(
+//                        join,
+//                        outerFeatures
+//                    );
+//
+//                    iterator2 = intersectedCollection2.features();
+//
+//                    list = new ArrayList<>();
+//
+//                    while (iterator2.hasNext()) {
+//                        feature2 = iterator2.next();
+//                        featureType = feature2.getFeatureType();
+//
+//                        list.add(feature2);
+//                    }
+//
+//                    intersectedCollection2 = new ListFeatureCollection(featureType, list);
+//
+//                    iteratorJoined2 = intersectedCollection2.features();
+//
+//                    System.out.println("Found intersected features with buildings: " + join.size());
+//
+//                    while (iteratorJoined2.hasNext()) {
+//                        feature = iteratorJoined2.next();
+//
+//                        String type;
+//
+//                        switch (feature.getAttribute(5).toString()) {
+//                            case "hd1":
+//                            case "hd2":
+//                            case "hd3":
+//                            case "hd4":
+//                            case "hd9":
+//                                type = "Hidro";
+//                                break;
+//                            case "ms0":
+//                                type = "Woods";
+//                                break;
+//                            case "ms4":
+//                                type = "Gardens";
+//                                break;
+//                            case "pu0":
+//                                type = "Buildings";
+//                                break;
+//                            default:
+//                                type = "Unknown";
+//                        }
+//
+//                        String key = feature.getAttribute("sven_SAV_P_SAV").toString() + "_" + type;
+//
+//                        if (!calculations.containsKey(key)) {
+//                            calculations.put(key, new FeatureInfo());
+//                        }
+//
+//                        FeatureInfo featureInfo = calculations.get(key);
+//
+//                        featureInfo.region = feature.getAttribute("sven_SAV_P_SAV").toString();
+//
+//                        featureInfo.type = type;
+//                        featureInfo.frequency++;
+//                        featureInfo.size += ((Geometry) feature.getDefaultGeometry()).getArea();
+//                        featureInfo.regionPlot += ((Geometry) feature2.getDefaultGeometry()).getArea();
+//                    }
+//                }
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        } finally {
+//            iterator.close();
+//        }
 
         String[][] data = new String[calculations.size()][6];
 
@@ -412,7 +537,6 @@ public class Calculations {
 //
 //        System.out.println("1: " + filteredFeaturesList.size());
 
-//        SimpleFeatureCollection filteredFeatures = DataUtilities.collection(filteredFeaturesList);
 //        SimpleFeatureCollection filteredFeatures = new ListFeatureCollection(featureType, filteredFeaturesList);
 
         IntersectionFeatureCollection intersection = new IntersectionFeatureCollection();
